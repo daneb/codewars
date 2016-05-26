77481dc84105b9280e9ad4d355c111566375df65
daneb <dane.balia@hetzner.co.za>
2016-05-26 08:52:39 +0200

    Added solution for double linear

--- efficiency_in_double_linear_sequence.rb
+++ efficiency_in_double_linear_sequence.rb
@@ -1,31 +1,63 @@
+# Initial Solution, even tried SortedSet
+
+# $my_array = [1]
+# $limit = 0
+# $counter = 0
+
+# def dbl_linear(n)
+#   generate_sequence(1) if $my_array.length == 1
+#   puts $my_array.length
+#   puts $my_array[$my_array.length - 1]
+#   $my_array[n]
+# end
+
+# def generate_sequence(n, counter = 1)
+#   35000.times {
+#     y = 2 * $my_array[$counter] + 1
+#     z = 3 * $my_array[$counter] + 1
+#     $my_array << y
+#     $my_array << z 
+#     $my_array[$counter, $counter + 1] = $my_array[$counter, $counter + 1]
+#     $counter = $counter + 1
+#   }
+#   $my_array.uniq!
+#   $my_array.sort_by! { |i| i }
+# end
+
 $my_array = [1]
-$limit = 0
-$counter = 0
+$hash = {} # for uniqueness
 
 def dbl_linear(n)
-  generate_sequence(1) if $my_array.length <= n
-  $my_array[n - 1]
+  generate_sequence(1) if $my_array.length == 1
+  puts $my_array.length
+  puts $my_array[$my_array.length - 1]
+  $my_array[n]
 end
 
-def generate_sequence(n, counter = 1)
-  9000.times {
-    y = 2 * $my_array[$counter] + 1
-    z = 3 * $my_array[$counter] + 1
-    $my_array << y unless $my_array.include?(y)
-    $my_array << z unless $my_array.include?(z)
-    $my_array[$counter, $counter + 1] = $my_array[$counter, $counter + 1].sort!
-    $counter = $counter + 1
+def generate_sequence(n)
+counter = 0
+  300000.times {
+    y = 2 * $my_array[counter] + 1
+    z = 3 * $my_array[counter] + 1
+    unless $hash[y]
+      $hash[y] = $my_array.length 
+      $my_array << y 
+    end
+    unless $hash[z]
+      $hash[z] = $my_array.length 
+      $my_array << z
+    end
+    counter = counter + 1
   }
+  $my_array.sort_by! { |i| i }
 end
 
-# def generate_sequence(n, counter = 0)
-#   return if $my_array.length >= $limit
-#   y = 2 * n + 1
-#   z = 3 * n + 1
-#   $my_array << y
-#   $my_array << z
-#   counter = counter + 1
-#   $my_array.uniq!
-#   $my_array.sort!
-#   generate_sequence($my_array[counter], counter)
+# Optimized Solution
+# def dbl_linear(n)
+#  array = [1]
+#  (n * n.size).times do |i|
+#     array << array[i] * 2 + 1
+#     array << array[i] * 3 + 1
+#   end
+#  array.uniq.sort[n]
 # end
